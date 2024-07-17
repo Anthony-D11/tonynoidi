916b1ccdfb629e788156c4c1cfb470719d7229dc
sprypoint-anthony-d11 <anguyen@sprypoint.com>
2024-07-17 02:13:15 -0300

    Update Homepage and its related css

--- src/components/Experience/experience.js
+++ src/components/Experience/experience.js
@@ -5,14 +5,14 @@ import Data from '../../assets/data.json';
 const workExperience = Data["work-experience"];
 var yearsOfExperience = Data["years-experience"];
 
-const cardColor = ["#FFF8F8", "#F3F5FF", "#EBF8F8"];
+const cardColor = ["#fff8cc", "#FFF8F8", "#F3F5FF", "#EBF8F8"];
 
 const Experience = () => {
     let work = [];
     for (let i = workExperience.length - 1; i >= 0; i--) {
         let logo = require('../../assets/' + workExperience[i]['image-name']);
         work.push(
-            <div className="col-md-3 experience-card">
+            <div className="col-lg-3 col-md-6 col-sm-6 col-6 experience-card">
                 <div className="card-style" style={{backgroundColor:cardColor[i]}}>
                     <div className="company-logo-container">
                         <img src={logo} alt="" className="company-logo"/>
@@ -29,14 +29,18 @@ const Experience = () => {
     <section className="section-section">
         <div className="container">
             <div className="row">
-                <div className="col-md-2 d-flex experience-title-container">
+                <div className="col-lg-2 col-md-12 col-sm-12 d-flex experience-title-container">
                     <div className="experience-title">
                         <div className="years-count">0<span>{yearsOfExperience}</span></div>
                         <h2>Years Experience in Field</h2>
                     </div>
                 </div>
-                <div className="col-md-1"></div>
-                {work}
+                <div className="col-lg-1"></div>
+                <div className="col-lg-9 col-md-12 col-sm-12">
+                    <div className="row">
+                        {work}
+                    </div>
+                </div>
             </div>
         </div>
     </section>
